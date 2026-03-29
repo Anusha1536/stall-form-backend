c59a5d07c57a4ab4f90584f2da263e229ca16d85
Anusha1536 <anushagaundalkar@gmail.com>
2026-03-30 01:39:30 +0530

    Cors issue fixed

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://eventstall-portal.netlify.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/stalls", stallRoutes);
