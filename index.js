c15a0b3eaa18e0190ae518a0fa0f9e35f9f87346
Anusha1536 <anushagaundalkar@gmail.com>
2026-03-30 20:29:19 +0530

    fixed bug

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ Explicitly handle preflight
-app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
